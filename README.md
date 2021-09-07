a36ea97c0554cdfa04b41d5e58559acdcc5ca946
fgudek <fgudek@student.unipu.hr>
2021-09-07 15:03:18 +0200

    1. commit
    Webapp Flask + fast.ai model

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Projekt Praksa
  Facial Expression Recognition
+
+# Requirements 
+
+Fast.ai library https://docs.fast.ai/install.html
+Dlib https://www.pyimagesearch.com/2017/03/27/how-to-install-dlib/
+Numpy
+Scipy
+IMutils
+OpenCV
+Pandas
+Argparse
+Python
+OpenCV
+
+# Change when run on your device 
+Path to .pkl file in camera.py and haarcascade_frontalface_default.xml has to be in working directory
---
+++ website/app.py
@@ -0,0 +1,262 @@
+from scipy.spatial import distance as dist
+import numpy as np
+import cv2
+from imutils import face_utils
+from fastai.vision import *
+from flask import Flask, render_template, Response, jsonify
+from camera import VideoCamera
+import imutils
+import argparse
+import time
+import dlib
+
+app = Flask(__name__)
+
+
+video_stream = VideoCamera()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(video_stream),
+                mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', debug=True,port="5000")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
